3532fb630979f89588cd0d448c6ecb68fee207db
Kyle Dreger <dregerkq@gmail.com>
2014-05-29 19:38:31 -0400

    apple web icon

--- _src/_posts/word-count.py
+++
@@ -1,6 +0,0 @@
-from glob import glob
-from os.path import isfile
-def countwords(fp):
-   with open(fp) as fh:
-       return len(fh.read().split())
-print sum(map(countwords, filter(isfile, glob("*.md") ) ) )
